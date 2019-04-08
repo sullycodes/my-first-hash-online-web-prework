82a25f3fef04c659bebde36a4efa40f7d76b084c
Daniel Sullivan <dansullivan19@gmail.com>
2019-04-08 21:05:41 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,7 +11,7 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
   the _manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3 }
   return
-
+  the manifest
 end
 
 def retrieval
